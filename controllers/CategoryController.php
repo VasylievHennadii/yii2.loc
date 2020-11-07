878e6754dc6c894b342cc5ce64a78f4ff3f0eb72
VasylievHennadii <vga.kha@gmail.com>
2020-11-07 23:27:01 +0200

    Output of product categories

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -18,5 +18,14 @@ class CategoryController extends AppController{
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();        
         return $this->render('index', compact('hits'));
     }
+
+    /**
+     * метод отвечает за показ той или иной категории
+     */
+    public function actionView($id){
+        $id = Yii::$app->request->get('id');
+        $products = Product::find()->where(['category_id' => $id])->all();
+        return $this->render('view', compact('products'));
+    }
     
 }
