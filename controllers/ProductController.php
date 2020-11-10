4f2cf144284e0661da8ee08c3d8303432cd03f89
VasylievHennadii <vga.kha@gmail.com>
2020-11-10 20:45:41 +0200

    Error processing

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 use app\models\Category;
 use app\models\Product;
 use Yii;
+use yii\web\HttpException;
 
 /**
  * Description of ProductController
@@ -15,6 +16,8 @@ class ProductController extends AppController{
     public function actionView($id){
         $id = Yii::$app->request->get('id');
         $product = Product::findOne($id); //ленивая загрузка
+        if(empty($product))
+            throw new HttpException(404, 'Такого товара нет.');
 //        $product = Product::find()->with('category')->where(['id' => $id])->limit(1)->one(); //жадная загрузка
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
         $this->setMeta('E-SHOPPER | ' . $product->name, $product->keywords, $product->description);
--- views/product/view.php
+++ views/product/view.php
@@ -317,12 +317,14 @@ use app\components\MenuWidget;
             <div class="col-sm-4">
                 <div class="product-image-wrapper">
                     <div class="single-products">
-                        <div class="productinfo text-center">                            
-                            <?= Html::img("@web/images/products/{$hit->img}", ['alt' => $hit->name])?>
-                            <h2>$<?=$hit->price;?></h2>
-                            <p><a href="<?= Url::to(['product/view', 'id' => $hit->id]) ?>"><?=$hit->name;?></a>></p>
-                            <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
-                        </div>
+                        <a href="<?=  Url::to(['product/view', 'id' => $hit->id])?>">
+                            <div class="productinfo text-center">                            
+                                <?= Html::img("@web/images/products/{$hit->img}", ['alt' => $hit->name])?>
+                                <h2>$<?=$hit->price;?></h2>
+                                <p><a href="<?= Url::to(['product/view', 'id' => $hit->id]) ?>"><?=$hit->name;?></a>></p>
+                                <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div> 
